c1db2b0976a0c056d1c441fa0068e41a3572417f
bryamslm <bryamlopez1882002@estudiantec.cr>
2025-01-17 22:51:25 -0600

    Fixed Video Class never used

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { video } from "framer-motion/client";
 import { FaGithub, FaExternalLinkAlt, FaYoutube } from "react-icons/fa";
 
 const projects = [
